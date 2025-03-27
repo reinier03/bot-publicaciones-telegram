c981fd216f03b6daaa73c97f47d33515b0f66a2c
reinier <reiniermayea@gmail.com>
2025-03-26 23:08:02 -0400

    lol

--- main.py
+++ main.py
@@ -477,13 +477,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
+        bot.process_new_updates([update])       
         
     return "Hello World"
 
